4d0aa102e647552d6c3109fd40b8ebfa0156857f
Orololuwa <67964734+Orololuwa@users.noreply.github.com>
2021-09-10 08:52:40 +0100

    added theme folder

---
+++ src/theme/colors.js
@@ -0,0 +1,16 @@
+export const white = {
+  100: "#FFFFFF"
+};
+
+export const grey = {
+  100: "#D9DBE9",
+  200: "#A0A3BD"
+};
+
+export const black = {
+  100: "#14142B"
+};
+
+export const blue = {
+  100: "#5F2EEA"
+};
