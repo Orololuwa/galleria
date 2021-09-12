037ba201e6907aed58a4e306e012ba51e32f9f74
Orololuwa <67964734+Orololuwa@users.noreply.github.com>
2021-09-12 12:37:34 +0100

    mansory layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,30 @@
 import Main from "./style";
 import React from "react";
 import { Thumbnail } from "models/Thumbnails";
+import Masonry from "react-masonry-css";
+
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1
+};
 
 const HomePage: React.FC<{ data: Thumbnail[] }> = (props) => {
-  console.log(props.data);
+  //console.log(props.data);
+
   return (
     <Main>
-      {props.data.map((img, i) => (
-        <img src={img.images.thumbnail} alt={img.name} key={i} />
-      ))}
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {props.data.map((img, i) => (
+          <img src={img.images.thumbnail} alt={img.name} key={i} />
+        ))}
+      </Masonry>
+      <section className="gallery"></section>
     </Main>
   );
 };
--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
-const Main = styled.main``;
+const Main = styled.main`
+  .my-masonry-grid {
+    display: -webkit-box; /* Not needed if autoprefixing */
+    display: -ms-flexbox; /* Not needed if autoprefixing */
+    display: flex;
+    margin-left: -30px; /* gutter size offset */
+    width: auto;
+    gap: 2.5rem;
+    padding: 0 2.5rem;
+  }
+  .my-masonry-grid_column {
+    padding-left: 30px; /* gutter size */
+    background-clip: padding-box;
+  }
+
+  /* Style your items */
+  .my-masonry-grid_column > img {
+    /* change div to reference your elements you put in <Masonry> */
+    background: grey;
+    margin-bottom: 30px;
+  }
+`;
 
 export default Main;
